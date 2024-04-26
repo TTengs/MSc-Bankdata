542663efe87d58d01106f6170ce03c5b55b4a5a4
TTengs <theistengs@gmail.com>
2024-04-26 19:24:26 +0200

    IBM MQ and DB2 connectors

--- code/CDC_Kafka/setup_connect.sh
+++ code/CDC_Kafka/setup_connect.sh
@@ -4,8 +4,16 @@ NO_COLOR='\033[0m'
 
 echo $COLOR_GREEN
 
-echo "Create connector for postgres"
+echo "Create connector for db2"
 
 echo $NO_COLOR
 
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8093/connectors/ -d @cdc/postgres_connector.json
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d @cdc/register-db2.json
+
+echo $COLOR_GREEN
+
+echo "Create connector for ibm mq"
+
+echo $NO_COLOR
+
+curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d @cdc/ibmmq_connector.json
